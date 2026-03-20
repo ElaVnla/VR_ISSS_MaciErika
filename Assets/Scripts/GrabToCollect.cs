2cef2ab6b209b6b53fe7e969e3ceda5c1ff3c04e
Erika Sampang <erika.sampang.c@gmail.com>
2026-03-20 15:01:31 -0500

    workingcollectingcube

---
+++ Assets/Scripts/GrabToCollect.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GrabToCollect : MonoBehaviour
+{
+    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable;
+
+    private void Awake()
+    {
+        grabInteractable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+    }
+
+    private void OnEnable()
+    {
+        grabInteractable.selectEntered.AddListener(OnGrab);
+    }
+
+    private void OnDisable()
+    {
+        grabInteractable.selectEntered.RemoveListener(OnGrab);
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        Debug.Log("Collected: " + gameObject.name);
+
+        Destroy(gameObject);
+    }
+}
