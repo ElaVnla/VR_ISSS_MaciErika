36f81bb65e7d327caacd8433392f7a67f9dcdf8f
Erika Sampang <erika.sampang.c@gmail.com>
2026-03-20 15:31:53 -0500

    specialeffects

--- Assets/Scripts/GrabToCollect.cs
+++ Assets/Scripts/GrabToCollect.cs
@@ -5,6 +5,9 @@ public class GrabToCollect : MonoBehaviour
 {
     private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable;
 
+    public GameObject collectEffectPrefab;
+    public AudioClip collectSound;
+
     private void Awake()
     {
         grabInteractable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
@@ -22,6 +25,19 @@ public class GrabToCollect : MonoBehaviour
 
     private void OnGrab(SelectEnterEventArgs args)
     {
+        // Spawn particle
+        if (collectEffectPrefab != null)
+        {
+            GameObject effect = Instantiate(collectEffectPrefab, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
+        }
+
+        // Play sound
+        if (collectSound != null)
+        {
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
+        }
+
         Debug.Log("Collected: " + gameObject.name);
 
         Destroy(gameObject);
